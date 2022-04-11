1cbc157f9c5a7b68ae02d5ca7edb94c089f53888
Blujedis <blujedicorp@gmail.com>
2022-04-11 12:53:46 -0700

    add back config require

--- src/atomic/ions/Icon.tsx
+++ src/atomic/ions/Icon.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { createIconSetFromIcoMoon } from 'react-native-vector-icons';
 
-import galioConfig from '../../config/galio.json';
 import getIconType, { IconFamilyType } from '../../helpers/getIconType';
 import GalioTheme, { withGalio } from '../../theme';
 import { BaseProps, InternalProps } from '../../types';
+
+const galioConfig = require('../../config/galio.json');
 
 export interface IconProps extends BaseProps {
   name?: string;
